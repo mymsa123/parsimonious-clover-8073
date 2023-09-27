a255195b572c354527cf0258e7089127682e5792
Deep-06 <shikhadeep.hzp@gmail.com>
2023-09-27 21:43:37 +0530

    day1

---
+++ medicare/src/Pages/Consultancy.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Consultancy = () => {
+  return (
+    <div>
+        <h1>Say Hello!</h1>
+        <h1>To a new era of healthcare.</h1>
+        <p>Now Book an Appointment to consult a specialist doctor online, anytime, anywhere.</p>
+    </div>
+  )
+}
+
+export default Consultancy
